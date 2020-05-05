a50baa66de206a11b48c10991745b20477bbbf68
felipefln <felipecarvalho_22_@hotmail.com>
2020-05-05 17:23:47 -0300

    add package cors and dotenv.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { GraphQLServer } from 'graphql-yoga'
 import resolvers from './resolvers/resolvers'
 import './database/db'
 import routes from './routes'
+const cors = require('cors')
 const app = express()
+require("dotenv").config();
 
 export const server = new GraphQLServer({
     typeDefs: path.resolve(__dirname, 'schema.graphql'),
@@ -17,7 +19,8 @@ export const server = new GraphQLServer({
 //     console.log('=> started ')
 //     return server
 // }
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.listen(3000)
+app.listen(process.env.SERVER_PORT || 3333)
 server.start()
